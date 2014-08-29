71ecff56f095f51fe84b804c91f8b57aa5d4c5b4
cai4feng <cai4feng@gmail.com>
2014-08-29 09:51:36 -0700

    Added OriginalDesign

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,25 @@
+int x=250;
+int y=250;
+int z=150;
+int a=150;
 void setup()
 {
-
+ size(500,500);
+ frameRate(10);
 }
 void draw()
-{
-  ellipse(50,50,50,50);
+{ 
+	
+	fill((int)(Math.random()*225),(int)(Math.random()*225),(int)(Math.random()*225));
+	noStroke();
+  	ellipse(x,y,z,a);
+  	ellipse(250,100,100,100);
+  	ellipse(250,400,100,100);
+  	ellipse(100,100,100,100);
+  
+  	ellipse(400,100,100,100);
+    
 }
 
+void mousePressed()
+{ background((int)(Math.random()*225),(int)(Math.random()*225),(int)(Math.random()*225));}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,31 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+int x=250;
+int y=250;
+int z=150;
+int a=150;
 public void setup()
 {
-
+ size(500,500);
+ frameRate(10);
 }
 public void draw()
-{
-  ellipse(50,50,50,50);
+{ 
+	
+	fill((int)(Math.random()*225),(int)(Math.random()*225),(int)(Math.random()*225));
+	noStroke();
+  	ellipse(x,y,z,a);
+  	ellipse(250,100,100,100);
+  	ellipse(250,400,100,100);
+  	ellipse(100,100,100,100);
+  
+  	ellipse(400,100,100,100);
+    
 }
 
+public void mousePressed()
+{ background((int)(Math.random()*225),(int)(Math.random()*225),(int)(Math.random()*225));}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
